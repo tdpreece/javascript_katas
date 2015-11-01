3d3f54a12a30e7e1f8fd2390ecd3c92c298616fc
Tim Preece <tdpreece@gmail.com>
2015-11-01 19:12:51 +0000

    Initialise array in a way that requires less typing and is more flexible to change.

--- bowling_scorer/angular_no_tests/bowling_scorer.js
+++ bowling_scorer/angular_no_tests/bowling_scorer.js
@@ -1,6 +1,6 @@
 var app = angular.module('BowlingScorerApp', []);
 app.controller('BowlingScorerController', function() {
-    this.rolls = [0, 0];
+    this.rolls = Array.apply(null, Array(2)).map(Number.prototype.valueOf,0);
     this.get_score = function() {
         return parseInt(this.rolls[0]) + parseInt(this.rolls[1]);
     }
