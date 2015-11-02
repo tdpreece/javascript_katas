2b972f34a09ca534f18573dd2d1f329cb8b2cc15
Tim Preece <tdpreece@gmail.com>
2015-11-02 21:52:15 +0000

    Added function to get score from user input.

--- bowling_scorer/angular_no_tests/bowling_scorer.js
+++ bowling_scorer/angular_no_tests/bowling_scorer.js
@@ -3,7 +3,11 @@ app.controller('BowlingScorerController', function() {
     this.rolls = Array.apply(null, Array(20)).map(Number.prototype.valueOf,0);
     this.get_score = function() {
         return this.rolls.reduce(function(a, b) {
-            return parseInt(a) +parseInt(b);
+            return get_roll_score(a) + get_roll_score(b);
         });
+    }
+
+    function get_roll_score(roll_value) {
+        return parseInt(roll_value)
     }
 });
