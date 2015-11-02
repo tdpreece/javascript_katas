606ca07024ad49a2396530711a20c7acdd903fa1
Tim Preece <tdpreece@gmail.com>
2015-11-02 21:49:14 +0000

    Revert "a is already an integer so don't need to conver it."
    This reverts commit efcb079ac93b12fa024ae633673fe8482299458e.

--- bowling_scorer/angular_no_tests/bowling_scorer.js
+++ bowling_scorer/angular_no_tests/bowling_scorer.js
@@ -3,7 +3,7 @@ app.controller('BowlingScorerController', function() {
     this.rolls = Array.apply(null, Array(20)).map(Number.prototype.valueOf,0);
     this.get_score = function() {
         return this.rolls.reduce(function(a, b) {
-            return a + parseInt(b);
+            return parseInt(a) +parseInt(b);
         });
     }
 });
